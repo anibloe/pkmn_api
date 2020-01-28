dd229f337250bc59362a519e6725123c9fa2fde3
anibloe <ashleynibloe@gmail.com>
2020-01-28 23:24:25 +0000

    Added random trading card image. Added moves and the effects of each move.

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 (function() {
-  var request = new XMLHttpRequest(),
+  var 
     names = [],
     ids = [],
-    endPoint = 'https://pokeapi.co/api/v2/pokemon/{id}?limit=2000&offset=0',
+    endPoints = {
+      'getInfo': 'https://pokeapi.co/api/v2/pokemon/{id}?limit=2000&offset=0',   // https://pokeapi.co/
+      'getMoveInfo': 'https://pokeapi.co/api/v2/move/{id}',
+      'getCard': 'https://api.pokemontcg.io/v1/cards?nationalPokedexNumber={id}' // https://docs.pokemontcg.io/
+    }, 
     dropdown = document.querySelector('select'),
-    image = document.querySelector('img');
+    sprite = document.querySelector('.sprite'),
+    card = document.querySelector('.card'),
+    movesContainer = document.querySelector('.move_container');
 
-  makeRequest(0, function(data) {
+  // Initialise the page on load
+  makeRequest('getInfo', false, function(data) {
 
+    // Prepare the arrays of ids and names
     data.results.forEach((pkmn) => {
       names.push(pkmn.name);
       ids.push(pkmn.url.split('/pokemon/')[1].split('/')[0]);
     });
   
+    // Create the dropdown
     names.forEach((name, index) => {
       var option = document.createElement('option');
   
@@ -22,34 +31,86 @@
       dropdown.appendChild(option);
     });
   
+    // Give dropdown functionality
     dropdown.addEventListener('change', function(e) {
       var id = e.target.value;
-      makeRequest(id, function(data) {
-        image.src = data.sprites.front_default;
+
+      // Clear the moves
+      movesContainer.innerHTML = '';
+
+      // Retrieve information
+      makeRequest('getInfo', id, function(data) {
+        var frontSprite = data.sprites.front_default,
+            moves = data.moves;
+
+        if (!frontSprite) {
+          sprite.classList.add('hide');
+        }
+        else {
+          sprite.src = frontSprite;
+          sprite.classList.remove('hide');
+        }
+
+        if (moves) {
+          moves.forEach(function(move) {
+            var moveId = move.move.url.split('/move/')[1].split('/')[0],
+                moveListItem = document.createElement('div');
+
+                moveListItem.setAttribute('data-move-id', moveId);
+                moveListItem.innerText = move.move.name;
+                
+                moveListItem.addEventListener('click', function() {
+                  makeRequest('getMoveInfo', moveId, function(data) {
+                    moveListItem.insertAdjacentHTML('beforeend', '<span> - ' + data.effect_entries[0].effect + '</span>');
+                  });
+                });
+
+            movesContainer.insertAdjacentElement('beforeend', moveListItem);
+          });
+        }
+
+        console.log(data);
+      });
+
+      // Retrieve the image of the playing card
+      makeRequest('getCard', id, function(data) {
+        var cards = data.cards;
+
+        if (cards.length > 0) {
+          var rng = Math.floor(Math.random() * Math.floor(cards.length));
+
+          card.src = data.cards[rng].imageUrl;
+          card.classList.remove('hide');
+        }
+        else {
+          card.classList.add('hide');
+        }
+        
         console.log(data);
       });
     });
   });
 
-  function makeRequest(id, callback) {
-    var ep;
+  function makeRequest(type, id, callback) {
+    var request = new XMLHttpRequest(),
+        ep;
 
-    ep = id ? endPoint.replace('{id}', id) : endPoint.replace('{id}', '');
+    // Insert the Pokemon's national Pokedex id into the url
+    ep = id ? endPoints[type].replace('{id}', id) : endPoints[type].replace('{id}', '');
 
+    // Open and send the request
     request.open('GET', ep, true);
-
+    request.send();
     request.onload = function() {
-      
       var data = JSON.parse(this.response);
     
       if (request.status >= 200 && request.status < 400) {
         callback(data);
       } 
       else {
-        console.log('error');
+        console.log('Error:' + request.status + " - " + request.statusText);
       }
     }
-    request.send();
   }
 
 })();
